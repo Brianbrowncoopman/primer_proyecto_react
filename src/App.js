2e5a0ddab93e3d36f498078b36b8ccbbb5e4677f
Brian Brown Coopman <brianbrowncoopman@gmail.com>
2022-08-24 00:41:07 -0400

    primer proyecto react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './Style.css';
+import HelloWorld from './HelloWorld';
+import Presentacion from './MiTarjeta';
+import { useState } from 'react';
+
+
+
 
 function App() {
+  const [name, setName] = useState('');
+
+  function onChange(e) {
+    setName(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hola {name}</p>
+      <label htmlFor='name'>Nombre</label>
+      <input type="text" name="name" onChange={onChange}/>
+      <HelloWorld />
+      <Presentacion />
     </div>
   );
 }
---
+++ src/MiTarjeta.js
@@ -0,0 +1,14 @@
+export default function Presentacion() {
+    return(
+    <div class="container">
+      <div class="Tarjeta">
+        <div class="nombre">  
+          <h1>Brian Brown</h1>
+        </div>
+        <div class="datos">
+          <h2>Hola Mundo</h2>
+        </div>
+      </div>
+    </div>   
+    );
+}
